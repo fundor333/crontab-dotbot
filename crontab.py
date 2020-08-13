5ef7110c5a6d2f015f214b33caf21c16356c1ceb
Fundor333 <fundor333@fundor333.com>
2020-08-13 23:40:40 +0200

    Fix minor

--- crontab.py
+++ crontab.py
@@ -50,16 +50,14 @@ class Crontab(dotbot.Plugin):
         try:
             is_skipped = False
             dummy_file = original_file + '.bak'
-            # Open original file in read only mode and dummy file in write mode
             with open(original_file, 'r+') as read_obj, open(dummy_file, 'w+') as write_obj:
-                # Line by line copy data from original file to dummy file
                 for line in read_obj:
-                    # If current line number matches the given line number then skip copying
-                    if self._comment in line:
+                    self._log.info(line)
+                    self._log.info(not self._comment in line)
+                    if not self._comment in line:
                         write_obj.write(line)
                     else:
                         is_skipped = True
-            # If any line is skipped then rename dummy file as original file
             if is_skipped:
                 os.remove(original_file)
                 os.rename(dummy_file, original_file)
